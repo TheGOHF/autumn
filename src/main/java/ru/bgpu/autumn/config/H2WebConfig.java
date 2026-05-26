af0be2855e731e0946b3fabbc76aba634a5b9b48
TheGOHF <Dbiytdsq14@yandex.ru>
2026-05-26 18:58:53 +0900

    last fixes (h2)

---
+++ src/main/java/ru/bgpu/autumn/config/H2WebConfig.java
@@ -0,0 +1,20 @@
+package ru.bgpu.autumn.config;
+
+import org.h2.server.web.JakartaWebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class H2WebConfig {
+
+    @Bean
+    public ServletRegistrationBean<JakartaWebServlet> h2ConsoleServlet() {
+        ServletRegistrationBean<JakartaWebServlet> bean =
+                new ServletRegistrationBean<>(new JakartaWebServlet());
+        bean.addUrlMappings("/h2-console/*");
+        return bean;
+    }
+}
